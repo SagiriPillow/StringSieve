e3514ee859fa8ae8c972160a6f1199898e0d3f14
UnknownMistyRain <70457081+UnknownMistyRain@users.noreply.github.com>
2020-09-26 14:58:52 +0800

    Create stringsieve.py

---
+++ stringsieve.py
@@ -0,0 +1,33 @@
+"""
+========
+StringSieve
+========
+
+:SourceCode:    `GitHub <https://github.com/UnknownMistyRain/StringSieve>`
+:License:     `MIT <https://github.com/UnknownMistyRain/StringSieve/blob/master/LICENSE>`
+"""
+
+
+def sieve(string: str, template: str, identifier: str = '*', ware: dict = None):
+    if ware is None:
+        ware: dict = {}
+    times: float = template.count(identifier)
+    times: float = times / 2
+    count: int = 0
+    while count != times:
+        start: int = template.find(identifier)
+        template: str = template[start + 1:]
+        end: int = template.find(identifier)
+        name: str = template[:end]
+        template: str = template[end + 1:]
+        last: int = template.find(identifier)
+        if last == -1:
+            last: None = None
+        behind: str = template[:last]
+        string: str = string[start:]
+        end: int = string.find(behind)
+        value: str = string[:end]
+        string: str = string[end:]
+        ware[name]: str = value
+        count: int = count + 1
+    return ware
